86ba912712eb677eea299657aa1f798849983f96
Javi <javsolgar@alum.us.es>
2019-11-13 20:29:27 +0100

    Almost finished feature create request

---
+++ src/main/java/acme/features/authenticated/request/AuthenticatedRequestCreateService.java
@@ -0,0 +1,75 @@
+
+package acme.features.authenticated.request;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.request.Request;
+import acme.framework.components.Errors;
+import acme.framework.components.Model;
+import acme.framework.entities.Authenticated;
+import acme.framework.services.AbstractCreateService;
+
+@Service
+public class AuthenticatedRequestCreateService implements AbstractCreateService<Authenticated, Request> {
+
+	//	Internal State -------------------------------------------------------------------------------------------------------------------
+	@Autowired
+	AuthenticatedRequestRepository repository;
+
+
+	// Abstract<Provider, Request> -------------------------------------------------------------------------------------------------------
+
+	@Override
+	public boolean authorise(final acme.framework.components.Request<Request> request) {
+		assert request != null;
+		return true;
+	}
+
+	@Override
+	public void bind(final acme.framework.components.Request<Request> request, final Request entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+		request.bind(entity, errors, "moment", "remember");
+
+	}
+
+	@Override
+	public void unbind(final acme.framework.components.Request<Request> request, final Request entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+		request.unbind(entity, model, "title", "description", "deadLine", "reward", "ticker");
+
+	}
+
+	@Override
+	public Request instantiate(final acme.framework.components.Request<Request> request) {
+		Request result;
+
+		result = new Request();
+		return result;
+	}
+
+	@Override
+	public void validate(final acme.framework.components.Request<Request> request, final Request entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+	}
+
+	@Override
+	public void create(final acme.framework.components.Request<Request> request, final Request entity) {
+		Date moment;
+
+		moment = new Date(System.currentTimeMillis() - 1);
+		entity.setMoment(moment);
+		this.repository.save(entity);
+
+	}
+
+}
--- src/main/webapp/META-INF/views/authenticated/request/form.jsp
+++ src/main/webapp/META-INF/views/authenticated/request/form.jsp
@@ -3,14 +3,23 @@
 <%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@taglib prefix="acme" tagdir="/WEB-INF/tags"%>
 
-<acme:form readonly="true">
+<acme:form>
+
 	<acme:form-textbox code="authenticated.request.form.label.title" path="title"/>
 	<acme:form-textarea code="authenticated.request.form.label.description" path="description"/>
-	<acme:form-moment code="authenticated.request.form.label.moment" path="moment"/>
+	<jstl:if test="${command != 'create'}">
+	<acme:form-moment code="authenticated.request.form.label.moment" path="moment"  readonly="true"/>
+	</jstl:if>
 	<acme:form-moment code="authenticated.request.form.label.deadline" path="deadLine"/>
 	<acme:form-money code="authenticated.request.form.label.reward" path="reward"/>
 	<acme:form-textbox code="authenticated.request.form.label.ticker" path="ticker"/>
+	<jstl:if test="${command == 'create'}">
+	<acme:message code="authenticated.request.form.message.explanation"/>
+	<acme:form-checkbox code="authenticated.request.form.checkbox.agree" path="remember"/>
+	</jstl:if>
 	
 	
+	
+	<acme:form-submit test ="${command == 'create'}" code="authenticated.request.form.button.create" action="/authenticated/request/create"/>
 	<acme:form-return code="authenticated.request.form.label.button.return"/>
 </acme:form>
