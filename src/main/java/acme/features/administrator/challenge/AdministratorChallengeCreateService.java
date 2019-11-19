e1af6ec51fff181201e385c4fd185ac9e666a7f1
Javi <javsolgar@alum.us.es>
2019-11-19 14:23:54 +0100

    validations doesn't work

--- src/main/java/acme/features/administrator/challenge/AdministratorChallengeCreateService.java
+++ src/main/java/acme/features/administrator/challenge/AdministratorChallengeCreateService.java
@@ -1,6 +1,8 @@
 
 package acme.features.administrator.challenge;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,7 @@ import acme.entities.challenge.Challenge;
 import acme.framework.components.Errors;
 import acme.framework.components.Model;
 import acme.framework.components.Request;
+import acme.framework.datatypes.Money;
 import acme.framework.entities.Administrator;
 import acme.framework.services.AbstractCreateService;
 
@@ -59,8 +62,24 @@ public class AdministratorChallengeCreateService implements AbstractCreateServic
 		assert entity != null;
 		assert errors != null;
 
+		Date dateNow, deadline;
+		boolean isFuture, hasDeadline;
 		boolean hasGoldGoal, hasSilverGoal, hasBronzeGoal;
+		boolean goldGoalFirst, goldSilverSecond, goldBronzeThird;
+		boolean hasGoldReward, hasSilverReward, hasBronzeReward;
+		Money rewardGold, rewardSilver, rewardBronze;
 
+		dateNow = new Date(System.currentTimeMillis() - 1);
+
+		hasDeadline = request.getModel().getString("deadline") != null && request.getModel().getString("deadline") != "";
+		errors.state(request, hasDeadline, "deadline", "administrator.challenge.error.must-have-deadline");
+
+		if (hasDeadline) {
+			deadline = new ;
+
+			isFuture = dateNow.before(deadline);
+			errors.state(request, isFuture, "deadline", "administrator.challenge.error.must-be-future");
+		}
 		hasGoldGoal = request.getModel().getString("goalGold") != null;
 		errors.state(request, hasGoldGoal, "goalGold", "administrator.challenge.error.must-have-goal");
 
@@ -69,6 +88,37 @@ public class AdministratorChallengeCreateService implements AbstractCreateServic
 
 		hasBronzeGoal = request.getModel().getString("goalBronze") != null;
 		errors.state(request, hasBronzeGoal, "goalGold", "administrator.challenge.error.must-have-goal");
+
+		hasGoldReward = request.getModel().getAttribute("rewardGold") != null && request.getModel().getAttribute("rewardGold") != "";
+		errors.state(request, hasGoldReward, "rewardGold", "administrator.challenge.error.must-have-reward");
+
+		hasSilverReward = request.getModel().getAttribute("rewardSilver") != null && request.getModel().getAttribute("rewardSilver") != "";
+		errors.state(request, hasSilverReward, "rewardSilver", "administrator.challenge.error.must-have-reward");
+
+		hasBronzeReward = request.getModel().getAttribute("rewardBronze") != null && request.getModel().getAttribute("rewardBronze") != "";
+		errors.state(request, hasBronzeReward, "rewardBronze", "administrator.challenge.error.must-have-reward");
+
+		if (hasGoldReward && hasSilverReward && hasBronzeReward) {
+
+			rewardGold = (Money) request.getModel().getAttribute("rewardGold");
+			rewardSilver = (Money) request.getModel().getAttribute("rewardSilver");
+			rewardBronze = (Money) request.getModel().getAttribute("rewardBronze");
+
+			goldGoalFirst = rewardGold.getAmount() >= rewardSilver.getAmount() && rewardGold.getAmount() >= rewardBronze.getAmount();
+			errors.state(request, goldGoalFirst, "rewardGold", "administrator.challenge.error.goldFirst");
+
+			if (goldGoalFirst) {
+
+				goldSilverSecond = rewardGold.getAmount() >= rewardSilver.getAmount() && rewardSilver.getAmount() >= rewardBronze.getAmount();
+				errors.state(request, goldSilverSecond, "rewardSilver", "administrator.challenge.error.silverSecond");
+
+				if (goldSilverSecond) {
+
+					goldBronzeThird = rewardGold.getAmount() >= rewardBronze.getAmount() && rewardSilver.getAmount() >= rewardBronze.getAmount();
+					errors.state(request, goldBronzeThird, "rewardBronze", "administrator.challenge.error.bronzeThird");
+				}
+			}
+		}
 
 	}
 
