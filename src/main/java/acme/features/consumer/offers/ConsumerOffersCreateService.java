85e75078dfef8cab9d3213028a5221a4ef44695c
Usuario <Usuario@DESKTOP-JDQOE0R.home>
2019-11-20 00:40:39 +0100

    revisando

--- src/main/java/acme/features/consumer/offers/ConsumerOffersCreateService.java
+++ src/main/java/acme/features/consumer/offers/ConsumerOffersCreateService.java
@@ -61,7 +61,7 @@ public class ConsumerOffersCreateService implements AbstractCreateService<Consum
 		assert request != null;
 		assert entity != null;
 		assert errors != null;
-		boolean isDuplicated, isRange, isAccepted, isFuture, hasDeadline, hasMajorRange, hasLowerRange;
+		boolean isDuplicated, isRange, isAccepted, isFuture, hasDeadline, hasTitle, hasDescription, hasMajorRange, hasLowerRange;
 		Date dateNow, deadline;
 
 		dateNow = new Date(System.currentTimeMillis() - 1);
@@ -75,20 +75,28 @@ public class ConsumerOffersCreateService implements AbstractCreateService<Consum
 			errors.state(request, isFuture, "deadline", "administrator.offers.error.must-be-future");
 
 		}
-
+		/*
+		 * 
+		 * No se si esto tambien debería estar
+		 * hasTitle = entity.getTitle() != null || entity.getTitle() != "";
+		 * errors.state(request, hasTitle, "title", "consumer.offers.error.notTitle");
+		 * 
+		 * hasDescription = entity.getDescription() != null || entity.getDescription() != "";
+		 * errors.state(request, hasDescription, "description", "consumer.offers.error.notDescription");
+		 */
 		isDuplicated = this.repository.findOneByTicker(entity.getTicker()) != null;
 		errors.state(request, !isDuplicated, "ticker", "consumer.offers.error.tickerDuplicated");
 
 		isRange = entity.getMajorRange().getAmount() > entity.getLowerRange().getAmount();
 		errors.state(request, isRange, "amountMajor", "consumer.offers.error.notRange");
 
-		hasMajorRange = entity.getMajorRange().getAmount() > entity.getLowerRange().getAmount();
+		hasMajorRange = entity.getMajorRange().getAmount() != null;
 		errors.state(request, hasMajorRange, "amountMajor", "consumer.offers.error.notRange");
 
-		hasLowerRange = entity.getMajorRange().getAmount() > entity.getLowerRange().getAmount();
+		hasLowerRange = entity.getLowerRange().getAmount() != null;
 		errors.state(request, hasLowerRange, "amountMajor", "consumer.offers.error.notRange");
 
-		isAccepted = request.getModel().getBoolean("accept");
+		isAccepted = request.getModel().getBoolean("accept") != null;
 		errors.state(request, isAccepted, "accept", "authenticated.offers.error.must-accept");
 
 	}
